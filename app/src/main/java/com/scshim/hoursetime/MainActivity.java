94d1db08da1d2c650d9c9f5c587ad58a406a7f66
simseoungchul <tlatmdcjf94@naver.com>
2020-05-19 14:51:55 +0900

    메인화면에서 각 세부화면으로 이동할 수 있는 버튼 추가

--- app/src/main/java/com/scshim/hoursetime/MainActivity.java
+++ app/src/main/java/com/scshim/hoursetime/MainActivity.java
@@ -1,7 +1,21 @@
 package com.scshim.hoursetime;
 
+
+/*
+개발자
+
+
+ */
+
+/*
+아이콘 제작자
+<a href="https://www.flaticon.com/kr/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon"> www.flaticon.com</a>
+<a href="https://www.flaticon.com/kr/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon"> www.flaticon.com</a>
+<a href="https://creativemarket.com/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/kr/" title="Flaticon"> www.flaticon.com</a>
+ */
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    void workAdd(View view){
+
     }
 }
--- app/src/main/java/com/scshim/hoursetime/domain/Work.java
+++ app/src/main/java/com/scshim/hoursetime/domain/Work.java
@@ -16,15 +16,18 @@ public class Work {
 
     //작업명
     String name;
-    //작업자 이름
-    String worker;
     //작업점수
     int score;
+
     //작업 완료 여부
-    boolean success;
+    boolean success = false;
+    //작업자 이름
+    String worker명 = "";
+    //작업 만족도. 1부터 5까지로 선택하며, 선택 값을 화면에 별로 표기한다.
+    int start = 0;
 
 
-    public Work(int startMonth, int startDay, int startHour, int startMinute, int endMonth, int endDay, int endHour, int endMinute, String name, String worker, int score, boolean success) {
+    public Work(int startMonth, int startDay, int startHour, int startMinute, int endMonth, int endDay, int endHour, int endMinute, String name, int score) {
         this.startMonth = startMonth;
         this.startDay = startDay;
         this.startHour = startHour;
@@ -34,8 +37,6 @@ public class Work {
         this.endHour = endHour;
         this.endMinute = endMinute;
         this.name = name;
-        this.worker = worker;
         this.score = score;
-        this.success = success;
     }
 }
