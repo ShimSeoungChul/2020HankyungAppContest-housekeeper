96d962ee0402005c7f2693cb2cbf73a6d58d47a0
simseoungchul <tlatmdcjf94@naver.com>
2020-05-24 15:51:01 +0900

    시작 버튼 오류 수정

--- app/src/main/java/com/scshim/hoursetime/StartActivity.java
+++ app/src/main/java/com/scshim/hoursetime/StartActivity.java
@@ -4,17 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button toMainBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-    }
 
-    void toMain(View view){
-        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(intent);//액티비티 생성
+        toMainBtn = findViewById(R.id.toMainBtn);
+        toMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);//액티비티 생성
+            }
+        });
     }
 }
